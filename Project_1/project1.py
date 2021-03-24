2d1344e132c783a28339e95a07767f2cd5645225
Taeuk Gwak <gwak2837@kakao.com>
2021-03-24 18:11:14 +0900

    Format codes and update the method of executing project

--- Project_1/project1.py
+++ Project_1/project1.py
@@ -3,30 +3,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 global img
-global num # image에 숫자를 찍기 위한 전역변수
+global num  # image에 숫자를 찍기 위한 전역변수
 num = 1
 
 global points
 points = []
 
+
 def save_point(x, y):
     point = [x, y]
     points.append(point)
+
 
 def get_point(event, x, y, flags, param):
     global num
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x, y), 6, (255, 255, 255), 3)
         save_point(x, y)
-        cv2.putText(img, str(num), (x-30, y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
+        cv2.putText(img, str(num), (x - 30, y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
         num = num + 1
+
 
 def get_patches(size):
     img_patches = []
     for i in range(4):
-        img_patches.append(image_1[points[i][1] - size: points[i][1] + size, points[i][0] - size: points[i][0] + size])
+        img_patches.append(
+            image_1[
+                points[i][1] - size : points[i][1] + size, points[i][0] - size : points[i][0] + size
+            ]
+        )
     for i in range(4, 8):
-        img_patches.append(image_2[points[i][1] - size: points[i][1] + size, points[i][0] - size: points[i][0] + size])
+        img_patches.append(
+            image_2[
+                points[i][1] - size : points[i][1] + size, points[i][0] - size : points[i][0] + size
+            ]
+        )
     return img_patches
 
 
@@ -35,8 +46,8 @@ def get_histogram(img_patches, bin):
     for i in range(8):
         dx = cv2.Sobel(img_patches[i], cv2.CV_32F, 1, 0, ksize=1)
         dy = cv2.Sobel(img_patches[i], cv2.CV_32F, 0, 1, ksize=1)
-        mag, angle = cv2.cartToPolar(dx, dy, angleInDegrees=True) # 벡터의 크기, 각도
-        #mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
+        mag, angle = cv2.cartToPolar(dx, dy, angleInDegrees=True)  # 벡터의 크기, 각도
+        # mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
 
         hist = cv2.calcHist([angle], [0], None, [bin], [0, 360])
         hist = hist.flatten()
@@ -47,35 +58,43 @@ def get_histogram(img_patches, bin):
 
 
 def draw_histogram(patch_hist, bin):
-    fig, ax = plt.subplots(2, 4, figsize=(20,15))
+    fig, ax = plt.subplots(2, 4, figsize=(20, 15))
     bin_x = np.arange(bin)
 
     for i in range(4):
-        ax[0, i].bar(bin_x, patch_hist[i], width=1, color='k')
-        ax[0, i].set_title("[image_1] NO." + str(i+1))
+        ax[0, i].bar(bin_x, patch_hist[i], width=1, color="k")
+        ax[0, i].set_title("[image_1] NO." + str(i + 1))
     for i in range(4):
-        ax[1, i].bar(bin_x, patch_hist[i+4], width=1, color='b')
-        ax[1, i].set_title("[image_2] NO." + str(i+5))
+        ax[1, i].bar(bin_x, patch_hist[i + 4], width=1, color="b")
+        ax[1, i].set_title("[image_2] NO." + str(i + 5))
     plt.show()
+
 
 def compare_histogram(patch_hist):
     result = []
     for a in range(4):
         temp_dist = []
-        for b in range(4,8):
+        for b in range(4, 8):
             # L2_Distance
-            temp_dist.append(np.linalg.norm(patch_hist[a]-patch_hist[b]))
-            print(np.linalg.norm(patch_hist[a]-patch_hist[b]))
+            temp_dist.append(np.linalg.norm(patch_hist[a] - patch_hist[b]))
+            print(np.linalg.norm(patch_hist[a] - patch_hist[b]))
         result.append(temp_dist.index(min(temp_dist)) + 3)
-    #print(result)
+    # print(result)
     return result
+
 
 def draw_match(result):
     match_img = np.hstack((copy_img1, copy_img2))
     for i in range(4):
-        match_img = cv2.line(match_img, (points[i][0], points[i][1]), (points[result[i]][0]+600, points[result[i]][1]), (0,0,0), 3)
+        match_img = cv2.line(
+            match_img,
+            (points[i][0], points[i][1]),
+            (points[result[i]][0] + 600, points[result[i]][1]),
+            (0, 0, 0),
+            3,
+        )
         # print('(',points[i][0], points[i][1],')', ' (',points[result[i]][0]+600, points[result[i]][1],')')
-    cv2.imshow('match_img', match_img)
+    cv2.imshow("match_img", match_img)
     cv2.waitKey(0)
 
 
@@ -83,14 +102,14 @@ def draw_match(result):
 image_1 = cv2.imread("1st.jpg")
 image_1 = cv2.cvtColor(image_1, cv2.COLOR_RGB2GRAY)
 image_1 = cv2.resize(image_1, (600, 800))
-cv2.namedWindow('image_1')
-cv2.setMouseCallback('image_1', get_point)
+cv2.namedWindow("image_1")
+cv2.setMouseCallback("image_1", get_point)
 
 # Get corner points to image_1
 copy_img1 = image_1.copy()
 img = copy_img1
-while(1):
-    cv2.imshow('image_1',copy_img1)
+while 1:
+    cv2.imshow("image_1", copy_img1)
     if cv2.waitKey(20) & 0xFF == 27:
         cv2.destroyAllWindows()
         break
@@ -100,19 +119,19 @@ while(1):
 image_2 = cv2.imread("2nd.jpg")
 image_2 = cv2.cvtColor(image_2, cv2.COLOR_RGB2GRAY)
 image_2 = cv2.resize(image_2, (600, 800))
-cv2.namedWindow('image_2')
-cv2.setMouseCallback('image_2', get_point)
+cv2.namedWindow("image_2")
+cv2.setMouseCallback("image_2", get_point)
 
 # Get corner points to image_2
 copy_img2 = image_2.copy()
 img = copy_img2
-while(1):
-    cv2.imshow('image_2',copy_img2)
+while 1:
+    cv2.imshow("image_2", copy_img2)
     if cv2.waitKey(20) & 0xFF == 27:
         cv2.destroyAllWindows()
         break
 
-print('points: ', points)
+print("points: ", points)
 
 # Get corner patches
 img_patches = get_patches(size=25)
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## 실행 방법
 
-#### 프로그램 설치
+### 프로그램 설치
 
 - Git v2.28
 - Python v3.9
 - Visual Studio Code
 
-#### 1. 프로젝트 다운로드
+### 프로젝트 다운로드
 
 ```shell
 > git clone https://github.com/CAUCV/CV_Project.git
@@ -18,18 +18,16 @@
 
 VSCode를 실행하면 오른쪽 아래에 '권장 확장 프로그램 설치' 알림이 뜬다.
 
-#### 2. 브랜치 이동 및 의존 패키지 설치
+### 프로젝트 실행
 
 ```shell
 > git checkout (브랜치 이름)
 > python3 -m pip install --upgrade pip
-> python3 -m pip install -r Project_1/requirements.txt
+> cd Project_1
+> python3 -m pip install -r requirements.txt
+> python3 project1.py
 ```
 
 이동하고 싶은 브랜치로 이동한 후 프로젝트에 필요한 외부 패키지를 설치한다.
 
-#### 3. OpenCV 실행
-
-```shell
-> python3 Project_1/project1.py
-```
+그리고 Python 3를 사용해서 OpenCV 프로젝트를 실행한다.
