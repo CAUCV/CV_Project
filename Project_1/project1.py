0198dafc0bfa248b10a3e09f37f256cc79800a41
YESOL PARK <47820809+yesoly@users.noreply.github.com>
2021-03-24 15:17:04 +0900

    Draw matching lines

--- Project_1/project1.py
+++ Project_1/project1.py
@@ -16,7 +16,7 @@ def save_point(x, y):
 def get_point(event, x, y, flags, param):
     global num
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), 6, (0, 0, 255), 3)
+        cv2.circle(img, (x, y), 6, (255, 255, 255), 3)
         save_point(x, y)
         cv2.putText(img, str(num), (x-30, y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
         num = num + 1
@@ -27,7 +27,6 @@ def get_patches(size):
         img_patches.append(image_1[points[i][1] - size: points[i][1] + size, points[i][0] - size: points[i][0] + size])
     for i in range(4, 8):
         img_patches.append(image_2[points[i][1] - size: points[i][1] + size, points[i][0] - size: points[i][0] + size])
-
     return img_patches
 
 
@@ -64,24 +63,26 @@ def compare_histogram(patch_hist):
     for a in range(4):
         temp_dist = []
         for b in range(4,8):
-            #temp_dist.append(cv2.compareHist(patch_hist[a], patch_hist[b], cv2.HISTCMP_BHATTACHARYYA))
-            #print(cv2.compareHist(patch_hist[a], patch_hist[b], cv2.HISTCMP_BHATTACHARYYA))
-
-            #temp_dist.append(cv2.compareHist(patch_hist[a], patch_hist[b], cv2.HISTCMP_CHISQR))
-            #print(cv2.compareHist(patch_hist[a], patch_hist[b], cv2.HISTCMP_CHISQR))
-
+            # L2_Distance
             temp_dist.append(np.linalg.norm(patch_hist[a]-patch_hist[b]))
             print(np.linalg.norm(patch_hist[a]-patch_hist[b]))
-        result.append(temp_dist.index(min(temp_dist)))
+        result.append(temp_dist.index(min(temp_dist)) + 3)
+    #print(result)
     return result
+
+def draw_match(result):
+    match_img = np.hstack((copy_img1, copy_img2))
+    for i in range(4):
+        match_img = cv2.line(match_img, (points[i][0], points[i][1]), (points[result[i]][0]+600, points[result[i]][1]), (0,0,0), 3)
+        # print('(',points[i][0], points[i][1],')', ' (',points[result[i]][0]+600, points[result[i]][1],')')
+    cv2.imshow('match_img', match_img)
+    cv2.waitKey(0)
+
 
 # Read Image1
 image_1 = cv2.imread("1st.jpg")
 image_1 = cv2.cvtColor(image_1, cv2.COLOR_RGB2GRAY)
-ratio = 600.0 / image_1.shape[1]
-dim = (600, int(image_1.shape[0] * ratio))
-image_1 = cv2.resize(image_1, dim)
-
+image_1 = cv2.resize(image_1, (600, 800))
 cv2.namedWindow('image_1')
 cv2.setMouseCallback('image_1', get_point)
 
@@ -94,12 +95,11 @@ while(1):
         cv2.destroyAllWindows()
         break
 
+
 # Read Image2
 image_2 = cv2.imread("2nd.jpg")
 image_2 = cv2.cvtColor(image_2, cv2.COLOR_RGB2GRAY)
-ratio = 600.0 / image_2.shape[1]
-dim = (600, int(image_2.shape[0] * ratio))
-image_2 = cv2.resize(image_2, dim)
+image_2 = cv2.resize(image_2, (600, 800))
 cv2.namedWindow('image_2')
 cv2.setMouseCallback('image_2', get_point)
 
@@ -122,6 +122,4 @@ bin = 36
 patch_hist = get_histogram(img_patches, bin)
 draw_histogram(patch_hist, bin)
 result = compare_histogram(patch_hist)
-print(result)
-for i in range(4):
-    print(i+1,"번,",result[i]+5,"번")
+draw_match(result)
